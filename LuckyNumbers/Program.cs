cf757120afd759115937c92d8c31b23c3d2ad7d1
mreilly29 <mreilly18@jcu.edu>
2018-09-15 11:43:48 -0400

    variables at top and added comments

--- LuckyNumbers/Program.cs
+++ LuckyNumbers/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace LuckyNumbers
 {
+    //Maggie Reilly 09/14/18
     class Program
     {
         static void Main(string[] args)
@@ -14,96 +15,83 @@ namespace LuckyNumbers
 
             while (playAgain == true) {
                 double jackpot = 100;
+                double div = 6;
+                Random rand = new Random();
+                int[] luckyNums = new int[6];               
+                int[] randNums = new int[6];
+                int correctNums = 0;
+                
+
                 Console.WriteLine("Welcome to Lucky Numbers! The jackpot today is $" + jackpot + " dollars!");
-                Console.WriteLine("First, you decide the range to guess your 6 lucky numbers!");
+                Console.WriteLine("You decide the range to guess your 6 lucky numbers!");
+
+                //user inputs starting and ending numbers for the range
                 Console.WriteLine("Please enter your starting number for the range.");
                 int startRange = int.Parse(Console.ReadLine());
-
                 Console.WriteLine("Please enter your ending number for the range.");
                 int endRange = int.Parse(Console.ReadLine());
 
-                //user inputs 6 lucky numbers and makes sure
-                Console.WriteLine("Enter your six lucky numbers. Do not duplicate any numbers");
-                int[] luckyNums = new int[6];
+                //user inputs 6 lucky numbers
+                Console.WriteLine("Enter your six lucky numbers within your range " + startRange + " to " + endRange + ".");
+                Console.WriteLine("Please do not duplicate any numbers");
+                //6 numbers saved in array
                 for (int i = 0; i < luckyNums.Length; i++)
                 {
                     luckyNums[i] = int.Parse(Console.ReadLine());
+                    //checks for valid number within the range set by user
                     if (luckyNums[i] < startRange || luckyNums[i] > endRange)
                     {
-                        Console.WriteLine("Please enter a number between the range: " + startRange + " to " + endRange);                       
+                        Console.WriteLine("Please enter a valid number between the range: " + startRange + " to " + endRange);                       
                         i--;
                     }
                 }
 
-                //generates random lucky numbers
-                Random rand = new Random();
-                int[] randNums = new int[6];
+                //generates six random lucky numbers               
                 for (int i = 0; i < randNums.Length; i++)
                 {
                     int randLucky = rand.Next(startRange, endRange + 1);
+                    //checks for duplicate random numbers
                     if (randNums.Contains(randLucky))
                     {
                         i--;
                     }
                     else
                     {
-                        randNums[i] = randLucky;
-                        Console.WriteLine("Lucky Number: " + randNums[i]);
+                        randNums[i] = randLucky;                        
                     }                                      
                 }
+                //displays the six random lucky numbers
+                foreach(int random in randNums)
+                {
+                    Console.WriteLine("Lucky Number: " + random);
+                }
 
-                //correctly guessed numbers
-                int correctNums = 0;
-                double div = 15;
+                //checks users guessed numbers to the randomly generated numbers
+                //number of correctly guessed numbers saved into int variable                
                 for (int i = 0; i < luckyNums.Length; i++)
                 {
                     if (luckyNums.Contains(randNums[i]))
                     {
                         correctNums++;
-                        div -= 2.5;
+                        div --;
                     }
                 }
-                Console.WriteLine("You guessed " + correctNums + " correctly!");
+                Console.WriteLine("You guessed " + correctNums + " numbers correctly!");
 
 
-                //user's winnings
-                double winnings = 0;
-                switch (correctNums)
+                //calculates user's winnings
+                double winnings = jackpot/div;
+                //if there are no correcly guessed numbers, winnings is $0
+                if (correctNums == 0)
                 {
-                    case (1):
-                        winnings = jackpot / div;
-                        break;
-
-                    case (2):
-                        winnings = jackpot / div;
-                        break;
-
-                    case (3):
-                        winnings = jackpot / div;
-                        break;
-
-                    case (4):
-                        winnings = jackpot / div;
-                        break;
-
-                    case (5):
-                        winnings = jackpot / div;
-                        break;
-
-                    case (6):
-                        winnings = jackpot;
-                        break;
-
-                    default:
-                        winnings = 0;
-                        break;
-                }
-
+                    winnings = 0;
+                }                
                 Console.WriteLine("You won $" + winnings + "!");
 
+                //asks user if they want to play again
                 Console.WriteLine("Would you like to play again? enter yes or no");
                 string input = Console.ReadLine().ToLower();
-                
+                //if input is no, the application ends
                 if(input == "no")
                 {
                     playAgain = false;
